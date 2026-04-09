95b1d1b12a11b51dfb410dc41f33c47a1bd387e1
devarajan-here <devarajanpm79@gmail.com>
2026-04-09 14:24:28 +0530

    Fix App Router crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import HeartMendApp from './pages/HeartMendApp';
 import UserDashboard from './pages/UserDashboard';
-import { SessionProvider } from './store/SessionStore';
 
 function App() {
   return (
-    <SessionProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<UserDashboard />} />
-          <Route path="/chat" element={<HeartMendApp />} />
-        </Routes>
-      </Router>
-    </SessionProvider>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<UserDashboard />} />
+      <Route path="/chat" element={<HeartMendApp />} />
+    </Routes>
   );
 }
 
